1e5e4a65f6df4eefa093c23e5d962e8f4d74e540
Fernando Mercadal <fermercadal@outlook.com>
2022-07-03 10:15:07 -0300

    Add active pad styles and display pressed sample name

--- components/Pad.tsx
+++ components/Pad.tsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import styles from "./Pad.module.css";
 
-const Pad = ({ sample }) => {
-  let padSound;
+const Pad = ({ sample, setPressed }) => {
+  const [playing, setplaying] = useState(false);
+
+  let padSound: any;
   if (typeof Audio !== "undefined") {
     padSound = new Audio(`/sounds/${sample.sound}.mp3`);
   }
 
   const playSound = () => {
+    setplaying(true);
+    setPressed(sample.padName);
     if (padSound) {
       padSound.currentTime = 0;
       padSound.play();
     }
+    setTimeout(() => {
+      setplaying(false);
+    }, 100);
   };
 
   return (
-    <button className={styles.Sampler__pad} onClick={playSound}>
+    <button
+      className={`${styles.Sampler__pad} ${
+        playing ? styles["Sampler__pad--active"] : ""
+      }`}
+      onClick={playSound}
+    >
       <span>{sample.padKey}</span>
     </button>
   );
